77957a4d7c36972f77123f9941b966c91d2bf747
Lars Tingelstad <lars.tingelstad@ntnu.no>
2022-01-26 14:15:30 +0100

    Stareted jointstate message

---
+++ sunrisedds/src/main/cpp/no_ntnu_mtp_ra_sunrisedds_msg_JointState.cpp
@@ -0,0 +1,95 @@
+#include "no_ntnu_mtp_ra_sunrisedds_msg_JointState.h"
+#include <dds/dds.h>
+#include "JointState.h"
+#include "sunrisedds_converters.h"
+#include "sunrisedds_signatures.h"
+
+JavaVM * g_vm = nullptr;
+
+sensor_msgs_msg_JointState *
+sensor_msgs_msg_JointState__convert_from_java(
+  jobject jmessage, sensor_msgs_msg_JointState * message)
+{
+  JNIEnv * env = nullptr;
+  assert(g_vm != nullptr);
+  g_vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6);
+  assert(env != nullptr);
+
+  if (message == nullptr) {
+    message = sensor_msgs_msg_JointState__alloc();
+  }
+
+  return message;
+}
+
+jobject
+sensor_msgs_msg_JointState__convert_to_java(
+  sensor_msgs_msg_JointState * message, jobject jmessage)
+{
+  JNIEnv * env = nullptr;
+  assert(g_vm != nullptr);
+  g_vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6);
+  assert(env != nullptr);
+
+  jclass jmessage_class = env->FindClass("no/ntnu/mtp/ra/sunrisedds/msg/JointState");
+  jmethodID jconstructor_mid = env->GetMethodID(jmessage_class, "<init>", "()V");
+  if (jmessage == nullptr) {
+    jmessage = env->NewObject(jmessage_class, jconstructor_mid);
+  }
+
+
+  return jmessage;
+}
+
+void
+sensor_msgs_msg_JointState__destroy(sensor_msgs_msg_JointState * message)
+{
+  sensor_msgs_msg_JointState_free(message, DDS_FREE_ALL);
+}
+
+JNIEXPORT jlong JNICALL
+Java_no_ntnu_mtp_ra_sunrisedds_msg_JointState_getFromJavaConverter(JNIEnv * env, jclass cls)
+{
+  (void)env;
+  (void)cls;
+  jlong ptr = reinterpret_cast<jlong>(&sensor_msgs_msg_JointState__convert_from_java);
+  return ptr;
+}
+
+JNIEXPORT jlong JNICALL
+Java_no_ntnu_mtp_ra_sunrisedds_msg_JointState_getToJavaConverter(JNIEnv * env, jclass cls)
+{
+  (void)env;
+  (void)cls;
+  jlong ptr = reinterpret_cast<jlong>(&sensor_msgs_msg_JointState__convert_to_java);
+  return ptr;
+}
+
+JNIEXPORT jlong JNICALL
+Java_no_ntnu_mtp_ra_sunrisedds_msg_JointState_getTopicDescriptor(JNIEnv * env, jclass cls)
+{
+  (void)env;
+  (void)cls;
+  jlong ptr = reinterpret_cast<jlong>(&sensor_msgs_msg_JointState_desc);
+  return ptr;
+}
+
+JNIEXPORT jlong JNICALL
+Java_no_ntnu_mtp_ra_sunrisedds_msg_JointState_getDestructor(JNIEnv * env, jclass cls)
+{
+  (void)env;
+  (void)cls;
+  jlong ptr = reinterpret_cast<jlong>(&sensor_msgs_msg_JointState__destroy);
+  return ptr;
+}
+
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM * vm, void *)
+{
+  // Can only call this once
+  if (g_vm == nullptr) {
+    g_vm = vm;
+  }
+
+  return JNI_VERSION_1_6;
+}
