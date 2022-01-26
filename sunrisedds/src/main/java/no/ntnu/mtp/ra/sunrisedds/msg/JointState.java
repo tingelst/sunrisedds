013d9384394c5c843579d345f041e5217befe4c7
Lars Tingelstad <lars.tingelstad@ntnu.no>
2022-01-26 14:15:20 +0100

    Added license header

--- sunrisedds/src/main/java/no/ntnu/mtp/ra/sunrisedds/msg/JointState.java
+++ sunrisedds/src/main/java/no/ntnu/mtp/ra/sunrisedds/msg/JointState.java
@@ -1,10 +1,22 @@
+// Copyright 2022 Norwegian University of Science and Technology.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package no.ntnu.mtp.ra.sunrisedds.msg;
-
-import no.ntnu.mtp.ra.sunrisedds.msg.MessageDefinition;
-import no.ntnu.mtp.ra.sunrisedds.utils.JNIUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import no.ntnu.mtp.ra.sunrisedds.utils.JNIUtils;
 
 public class JointState implements MessageDefinition {
     private static final Logger logger = LoggerFactory.getLogger(JointState.class);
@@ -12,7 +24,7 @@ public class JointState implements MessageDefinition {
     static {
         try {
             JNIUtils.loadImplementation(JointState.class);
-        } catch (UnsatisfiedLinkError ule) {
+        } catch (final UnsatisfiedLinkError ule) {
             logger.error("Native code library failed to load.\n" + ule);
             System.exit(1);
         }
@@ -24,47 +36,49 @@ public class JointState implements MessageDefinition {
     private double[] velocity;
     private double[] effort;
 
-    public JointState() {
-    }
-
     public Header getHeader() {
         return header;
     }
 
-    public void setHeader(Header header) {
+    public JointState setHeader(final Header header) {
         this.header = header;
+        return this;
     }
 
     public String[] getName() {
         return name;
     }
 
-    public void setName(String[] name) {
+    public JointState setName(final String[] name) {
         this.name = name;
+        return this;
     }
 
     public double[] getPosition() {
         return position;
     }
 
-    public void setPosition(double[] position) {
+    public JointState setPosition(final double[] position) {
         this.position = position;
+        return this;
     }
 
     public double[] getVelocity() {
         return velocity;
     }
 
-    public void setVelocity(double[] velocity) {
+    public JointState setVelocity(final double[] velocity) {
         this.velocity = velocity;
+        return this;
     }
 
     public double[] getEffort() {
         return effort;
     }
 
-    public void setEffort(double[] effort) {
+    public JointState setEffort(final double[] effort) {
         this.effort = effort;
+        return this;
     }
 
     public static native long getDestructor();
@@ -90,7 +104,5 @@ public class JointState implements MessageDefinition {
     public long getTopicDescriptorInstance() {
         return JointState.getTopicDescriptor();
     }
-
-    public static native long convertFromJava(JointState msg);
 
 }
